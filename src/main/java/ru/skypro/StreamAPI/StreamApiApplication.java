ac901fe137588036bd6039f6e438c6b8232d462c
HomeMedia <taknado13@yandex.ru>
2024-05-30 02:16:38 +0700

    start

---
+++ src/main/java/ru/skypro/StreamAPI/StreamApiApplication.java
@@ -0,0 +1,13 @@
+package ru.skypro.StreamAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamApiApplication.class, args);
+	}
+
+}
